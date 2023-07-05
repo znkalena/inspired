188b09d21351308204eb7b1d64f6b047e96b8bc2
znkalena <elze66@gmail.com>
2023-07-05 22:40:33 +0300

    error

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,21 +3,28 @@ import { Container } from '../Layout/Container/container'
 import s from './Footer.module.scss'
 import { NavLink } from 'react-router-dom'
 const list = [
-    {link:'women',title :'Женщины',catigories:[
-        {link:'bras',title:'Бюстгальтеры'},
-        {link:'panties',title:'Трусы'},
-        {link:'socks',title:'Носки'},
-        {link:'bathrobes',title:'Халаты'},
-        {link:'thermal',title:'Термобелье'},
-        {link:'pigamas',title:'Пижамы'}
+    {
+    link:'women',
+    title :'Женщины',
+    catigories:[
+        {link:"bras",title:"Бюстгальтеры"},
+        {link:"panties",title:"Трусы"},
+        {link:"socks",title:"Носки"},
+        {link:"bathrobes",title:"Халаты"},
+        {link:"thermal",title:"Термобелье"},
+        {link:"pigamas",title:"Пижамы"},
     ]},
-    {link:'men',title :'Мужчины',categories:[        
-        {link:'panties',title:'Трусы'},
-        {link:'socks',title:'Носки'},
-        {link:'bathrobes',title:'Халаты'},
-        {link:'thermal',title:'Термобелье'}        
+    {
+    link:'men',
+    title :'Мужчины',
+    categories:[        
+        {link:"panties",title:"Трусы"},
+        {link:"socks",title:"Носки"},
+        {link:"bathrobes",title:"Халаты"},
+        {link:"thermal",title:"Термобелье"}        
     ]}
 ]
+
 export const Footer =() => (
     <footer >
         <Container>
@@ -25,20 +32,17 @@ export const Footer =() => (
                 <div className={s.category}>
                     <h2 className={classNames(s.title,s.categoryTitle)}>Каталог</h2>
                     <ul className={s.categoryList}>
-                        {list.map(item =>(
-                            <li key={item.link} className={s.categoryItem}>{item.link}
-                            <h3 className={s.categorySubtitle}>{item.title}
-                            <NavLink to={item.link}  className={s.link}></NavLink>
+                        {list.map(item =>{
+                            <li key={item.link} className={s.categoryItem}>
+                            <h3 className={s.categorySubtitle}>
+                            <NavLink to={item.link}  className={s.link}>{item.title}</NavLink>
                             </h3>
                             <ul className={s.categorySublist}>
-                                {item.categories.map(category =>(
-                                    <li key={category.link} >
-                                    <NavLink className={s.link} to={`${item.link}/${category.link}`}>{category.title}</NavLink>
-                                    </li>
-                                ))}
+                            {console.log(item.categories)}
+                                            
                             </ul>
                             </li>                        
-                        ))}
+                        })}
                     </ul>
                 </div>
             </div>
