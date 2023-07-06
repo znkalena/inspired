38a7edeb2760add1df6199e37d852627afeb102c
znkalena <elze66@gmail.com>
2023-07-06 09:26:55 +0300

    make homework

--- src/components/header/Navigation/Category/Category.jsx
+++ src/components/header/Navigation/Category/Category.jsx
@@ -1,16 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import s from './Category.module.scss'
 import classNames from 'classnames';
 
 
-export const Category = ({list}) => (
-    <ul className={s.category}>
-{list.map(item =>(
-<li key={item.link} className={s.link}>
+export const Category = ({list}) =>{
+    const location = useLocation();
+    if(location.pathname==='women'){
+    const listWomen =list[0];
+<ul className={s.category}>
+{listWomen.categories.map(category =>(
+<li key={category.link}>
     <NavLink className={({isActive}) => classNames(s.link,isActive && s.linkActive)}
-        to={item.link}> {item.title}</NavLink>
+        to={category.link}> {category.title}</NavLink>
 </li>
 ))
 }
     </ul> 
-    );
+    }else{
+        const listMen =list[1];
+        <ul className={s.category}>
+        {listMen.categories.map(category =>(
+        <li key={category.link}>
+            <NavLink className={({isActive}) => classNames(s.link,isActive && s.linkActive)}
+                to={category.link}> {category.title}</NavLink>
+        </li>
+        ))
+        }
+            </ul>    
+    }
+    
+};
